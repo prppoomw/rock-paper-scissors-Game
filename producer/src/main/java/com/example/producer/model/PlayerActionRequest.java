2e4cd725ac5ea0761e6ea15be2188874617f2c05
prppoomw <poomrapeewon@gmail.com>
2025-01-09 14:04:36 +0700

    test(service): add simple service test

--- producer/src/main/java/com/example/producer/model/PlayerActionRequest.java
+++ producer/src/main/java/com/example/producer/model/PlayerActionRequest.java
@@ -3,9 +3,11 @@ package com.example.producer.model;
 import com.example.producer.enums.RpsEnum;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PlayerActionRequest {
     @NotNull
     @NotEmpty
