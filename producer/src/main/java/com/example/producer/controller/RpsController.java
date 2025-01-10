5e87a603265d472a947ce93896fc2250d33ae5fe
prppoomw <poomrapeewon@gmail.com>
2025-01-10 18:46:20 +0700

    fix: handle error & request validate

--- producer/src/main/java/com/example/producer/controller/RpsController.java
+++ producer/src/main/java/com/example/producer/controller/RpsController.java
@@ -2,10 +2,14 @@ package com.example.producer.controller;
 
 import com.example.producer.model.PlayerActionRequest;
 import com.example.producer.service.RpsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/rps-producer")
@@ -15,7 +19,12 @@ public class RpsController {
     private RpsService rpsService;
 
     @PostMapping("/play")
-    public ResponseEntity<?> playGame(@RequestBody PlayerActionRequest playerActionRequest) {
-        return rpsService.playGame(playerActionRequest);
+    public ResponseEntity<?> playGame(@Validated @RequestBody PlayerActionRequest playerActionRequest) {
+        try {
+            return rpsService.playGame(playerActionRequest);
+        } catch (Exception e) {
+            log.error("error: {}", e.getMessage(), e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
